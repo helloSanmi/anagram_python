6cbafd8392f0ada4dd275fa49b2fd24f56e808b1
Oluwasanmi Idowu <95680393+helloSanmi@users.noreply.github.com>
2022-06-10 09:29:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,14 @@
 def find_anagram(word, anagram):
 
     if sorted(word.lower()) == sorted(anagram.lower()):
+        print('Program return True')
         return True
 
+    print('Program return False')
     return False
 
 
+user_input = input('Enter first word: ')
+user_input_second = input('Enter second wor: ')
+
+find_anagram(user_input, user_input_second);
